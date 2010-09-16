78831b28bbd6cb381ec9db039b97290769bae033
Kor de Jong <k.dejong@geo.uu.nl>
2010-09-16 08:23:25 -0700

    Commented out dependency on psutil.
    Automatic installation of psutil and matplotlib doesn't seem to work on
    Windows.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 
 setup(
-  install_requires=["psutil>=0.1.3"],
+  # install_requires=["psutil>=0.1.3"], And matplotlib. Doesn't work on Windows.
   name = "PerformanceAnalyst",
   version = "0.0.5",
   package_dir = {"": "Sources"},
