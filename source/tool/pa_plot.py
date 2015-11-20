af05aefe5a84203ea19294eeaabe24a9f6aecbff
Kor de Jong <k.dejong1@uu.nl>
2015-11-20 16:09:46 +0100

    Add --real and --cpu

--- source/tool/pa_plot.py
+++ source/tool/pa_plot.py
@@ -1,15 +1,20 @@
 """
 Create a pdf with a plot of all timers supplied
 
-usage: pa plot [--timestamp=<timestamp>] <output> <database> <timer>...
+usage: pa plot [--real] [--cpu] [--timestamp=<timestamp>]
+    <output> <database> <timer>...
 
 options:
+    --real                      Plot real (user) times
+    --cpu                       Plot cpu times
     --timestamp=<timestamp>     Timestamp to select (yyyy-mm-dd, 'latest'
                                 or -1, -2, -3, etc), default: today
 
 arguments:
     database                    Name of database
     timer                       Name of timer
+
+In case --real and --cpu are not passed, both real and cpu times are plotted.
 """
 import datetime
 import os.path
@@ -78,6 +83,7 @@ def plot(
         database_name,
         timer_names,
         timestamp,
+        indicators,
         output_pathname):
 
     assert os.path.isfile(database_name), \
@@ -112,27 +118,47 @@ def plot(
             cpu_times[name][timestamp], algorithm="std") for timestamp in
             sorted(cpu_times[name])]
 
+    # http://stackoverflow.com/questions/4805048/how-to-get-different-colored-lines-for-different-plots-in-a-single-figure/4805456#4805456
+    nr_timers = len(timer_names)
+    nr_plots = nr_timers
+    # http://matplotlib.org/1.2.1/examples/pylab_examples/show_colormaps.html
+    colormap = pyplot.cm.brg
+    colors = [colormap(i) for i in numpy.linspace(0, 0.9, nr_plots)]
+    labels = []
+
     # Plot mean timing.
-    for name in timer_names:
-        # Store properties of plot with real-time values.
-        lines[name] = axis.plot(sorted(real_times[name]),
-            real_times_mean[name], "o--")[0]
-        # axis.plot(sorted(cpu_times[name]), cpu_times_mean[name], "o-",
-        #     color=lines[name].get_color())
+    # for name in timer_names:
+    for i in xrange(nr_timers):
+        timer_name = timer_names[i]
+        labels.append(timer_name)
+
+        if "real" in indicators:
+            axis.plot(sorted(real_times[timer_name]),
+                real_times_mean[timer_name], "o--", color=colors[i],
+                label="{} (real)".format(timer_name))
+
+        if "cpu" in indicators:
+            axis.plot(sorted(cpu_times[timer_name]),
+                cpu_times_mean[timer_name], "o-", color=colors[i],
+                label="{} (cpu)".format(timer_name))
 
     # Plot error bars.
-    for name in timer_names:
-        if len(real_times_mean[name]) > 0:
-            axis.errorbar(sorted(real_times[name]), real_times_mean[name],
-                real_times_std[name], fmt="o",
-                color=lines[name].get_color())
-        # if len(cpu_times_mean[name]) > 0:
-        #     axis.errorbar(sorted(cpu_times[name]), cpu_times_mean[name],
-        #         cpu_times_std[name], fmt="o",
-        #         color=lines[name].get_color())
+    for i in xrange(nr_timers):
+        timer_name = timer_names[i]
 
-    legend = pyplot.figlegend([lines[name] for name in timer_names],
-        timer_names, "upper right") # loc=(0.0, 0.0))
+        if "real" in indicators:
+            if len(real_times_mean[timer_name]) > 0:
+                axis.errorbar(sorted(real_times[timer_name]),
+                    real_times_mean[timer_name], real_times_std[timer_name],
+                    fmt="o", color=colors[i])
+
+        if "cpu" in indicators:
+            if len(cpu_times_mean[timer_name]) > 0:
+                axis.errorbar(sorted(cpu_times[timer_name]),
+                    cpu_times_mean[timer_name], cpu_times_std[timer_name],
+                    fmt="o", color=colors[i])
+
+    legend = pyplot.legend(loc="upper right", fancybox=True, shadow=True)
 
     for text in legend.get_texts():
         text.set_fontsize("small")
@@ -152,6 +178,18 @@ def plot(
 @util.checked_call
 def run(
         arguments):
+    plot_real_times = arguments["--real"]
+    plot_cpu_times = arguments["--cpu"]
+
+    if not plot_real_times and not plot_cpu_times:
+        plot_real_times, plot_cpu_times = True, True
+
+    indicators = []
+    if plot_real_times:
+        indicators.append("real")
+    if plot_cpu_times:
+        indicators.append("cpu")
+
     timestamp = util.string_to_timestamp(arguments["--timestamp"]) if \
         arguments["--timestamp"] else datetime.datetime.combine(
             datetime.date.today(), datetime.datetime.min.time())
@@ -159,7 +197,7 @@ def run(
     database_name = arguments["<database>"]
     timer_names = arguments["<timer>"]
 
-    plot(database_name, timer_names, timestamp, output_pathname)
+    plot(database_name, timer_names, timestamp, indicators, output_pathname)
 
 
 if __name__ == "__main__":
